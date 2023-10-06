b6eef14f59f4703f99dcb008395d322c5efe350a
Молчанов Денис <dnb2011@yandex.ru>
2023-10-07 00:53:53 +0300

    fix: ошибки постман

--- ewm-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
@@ -24,7 +24,7 @@ import ru.practicum.utils.PageConfig;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Transactional(readOnly = true)
+@Transactional
 @Service
 @Slf4j
 @RequiredArgsConstructor
